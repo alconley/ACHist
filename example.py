5dca71e63c3f7acb75aec1061d27146e51899f72
alconley <imaconley1@gmail.com>
2023-09-11 08:56:46 -0400

    modified:   example.py

--- example.py
+++ example.py
@@ -11,20 +11,25 @@ df = pl.read_parquet("52Cr_July2023_REU_CeBrA/built/run_83.parquet")
 
 h = Histogrammer() # create an instance of the Histogrammer class
 
-# # plot a subplot with the focal plane spectrum and the PID without a cut
-fig_NoCut, ax_NoCut = plt.subplots(2,1)
-h.histo2d(xdata=[df["ScintLeftEnergy"],df["ScintLeftEnergy"]],ydata=[df["AnodeBackEnergy"],df["AnodeBackEnergy"]], bins=[512,512], range=[ [0,2048], [0,2048]], subplots=(fig_NoCut, ax_NoCut[0]), cbar=False) #Particle Identification Plot
-h.histo2d(xdata=[df["ScintLeftEnergy"]],ydata=[df["AnodeBackEnergy"]], bins=[512,512], range=[ [0,2048], [0,2048]], cbar=False) #Particle Identification Plot
 
-h.histo1d(xdata=df["Xavg"], bins=600, range=(-300,300), subplots=(fig_NoCut, ax_NoCut[1])) # Spectrum without cut
+def NoCutPlotExample():
+    # plot a subplot with the focal plane spectrum and the PID without a cut
+    fig_NoCut, ax_NoCut = plt.subplots(2,1)
+    h.histo2d(xdata=[df["ScintLeftEnergy"],df["ScintLeftEnergy"]],ydata=[df["AnodeBackEnergy"],df["AnodeBackEnergy"]], bins=[512,512], range=[ [0,2048], [0,2048]], subplots=(fig_NoCut, ax_NoCut[0]), cbar=False) #Particle Identification Plot
+    h.histo2d(xdata=[df["ScintLeftEnergy"]],ydata=[df["AnodeBackEnergy"]], bins=[512,512], range=[ [0,2048], [0,2048]], cbar=False) #Particle Identification Plot
+    plt.show()
 
-# # filter the data frame with the proton cut created with the 2D histogram example
-# PID_df = h.filter_df_with_cut(df=df, XColumn="ScintLeftEnergy", YColumn="AnodeBackEnergy", CutFile="./histogrammer/example_cut.json")
 
-# # # plot a subplot with the focal plane spectrum and the PID with a cut
-# fig_Cut, ax_Cut = plt.subplots(2,1)
-# h.histo2d(xdata=[PID_df["ScintLeftEnergy"]], ydata=[PID_df["AnodeBackEnergy"]], bins=[512,512], range=[ [0,2048], [0,2048]], subplots=(fig_Cut, ax_Cut[0]), cbar=False) #Particle Identification Plot
-# h.histo1d(xdata=PID_df["Xavg"], bins=600, range=(-300,300), subplots=(fig_Cut, ax_Cut[1])) # Spectrum without cut
+def CutPlotExample(CutFile):
+    # filter the data frame with the proton cut created with the 2D histogram example
+    PID_df = h.filter_df_with_cut(df=df, XColumn="ScintLeftEnergy", YColumn="AnodeBackEnergy", CutFile="./histogrammer/example_cut.json")
+
+    # # plot a subplot with the focal plane spectrum and the PID with a cut
+    fig_Cut, ax_Cut = plt.subplots(2,1)
+    h.histo2d(xdata=[PID_df["ScintLeftEnergy"]], ydata=[PID_df["AnodeBackEnergy"]], bins=[512,512], range=[ [0,2048], [0,2048]], subplots=(fig_Cut, ax_Cut[0]), cbar=False) #Particle Identification Plot
+    h.histo1d(xdata=PID_df["Xavg"], bins=600, range=(-300,300), subplots=(fig_Cut, ax_Cut[1])) # Spectrum without cut
+
+    plt.show()
 
 
 # df = pl.read_parquet("52Cr_July2023_REU_CeBrA/analysis/run_83_112_gainmatched.parquet")
@@ -37,5 +42,11 @@ h.histo1d(xdata=df["Xavg"], bins=600, range=(-300,300), subplots=(fig_NoCut, ax_
 
 # h.histo1d(xdata=df["XavgEnergyCalibrated"], bins=300, range=(0,6000))
 
+if __name__ == "__main__":
+    
+    NoCutPlotExample
+    
+    CutFile="./histogrammer/example_cut.json"
 
-plt.show()
+    CutPlotExample(CutFile=CutFile)
+    
